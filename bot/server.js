2038dad9a2d5a103f12dbe7cd2da6668a683b98e
arthuroliveiraa <arthur.oliveira@cnecuberaba.edu.br>
2020-07-27 16:07:28 -0300

    Bot

--- bot/server.js
+++ bot/server.js
@@ -18,25 +18,3 @@ socket.on("connected", (socket) => {
 })
 
 socket.emit("bot add");
-socket.on("stock", (stock) => {
-    request.get(`https://stooq.com/q/l/?s=${stock}&f=sd2t2ohlcv&h&e=csv`, {}, (error, response) => {
-        const [headersLine, ...dataLines] = response.body.split("\r\n");
-        const properties = headersLine.split(",");
-        const stocks = dataLines.map((aStockString) => {
-            if (aStockString != "") {
-                const splittedStockString = aStockString.split(",");
-                return properties.reduce(
-                    (aStock, aProperty, anIndex) => {
-                        aStock[aProperty] = splittedStockString[anIndex];
-                        return aStock;
-                    },
-                    {}
-                );
-            }
-        }, []);
-        if (Number(stocks[0].Close))
-            socket.send(`${stocks[0].Symbol} quote is ${stocks[0].Close} per share`);
-        else
-            socket.send(`${stock.toUpperCase()} quote could not be processed or was not found`);
-    });
-});
